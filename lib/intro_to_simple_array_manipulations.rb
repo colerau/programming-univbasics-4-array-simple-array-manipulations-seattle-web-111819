3bcfe942b608713b7a6ad1821029f6887c9f8f27
Cole Rau <colevrau@gmail.com>
2019-10-03 22:18:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,12 @@
 def using_push(array, string)
   array.unshift(string)
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array, string)
+  array.pop(string)
+end
+
